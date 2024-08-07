ddf656449b35d91858f958ab8576d898949d6e33
Roeland <Roeland.Lutters@telenet.be>
2024-08-07 11:55:11 +0200

    fix radom serial numbers

--- sma/src/homewizard.py
+++ sma/src/homewizard.py
@@ -1,12 +1,11 @@
 from zeroconf import Zeroconf, ServiceBrowser, ServiceStateChange
-import socket
 from config import settings
 import logging
 import requests
+import hashlib
 from emeter import emeterPacket
 import time
 import json
-import random
 
 def setup_homewizard(userdata):
     if settings.get("enable_homewizard", False) is False:
@@ -14,9 +13,8 @@ def setup_homewizard(userdata):
     
     zeroconf = Zeroconf()
     browser = ServiceBrowser(zeroconf, "_hwenergy._tcp.local.", handlers=[lambda zeroconf, service_type, name, state_change: on_service_state_change(zeroconf, service_type, name, state_change, userdata)])
-    random.seed(42)
     for ip in settings.get("homewizard_manual_addresses", []):
-        userdata['homewizard_meters'][ip] = hash(ip)
+        userdata['homewizard_meters'][ip] = string_to_int(ip)
 
 def on_service_state_change(zeroconf, service_type, name, state_change, userdata):
     if state_change is ServiceStateChange.Added:
@@ -26,8 +24,7 @@ def on_service_state_change(zeroconf, service_type, name, state_change, userdata
             hostname = info.server
             logging.debug(f"Found device with hostname from ServiceInfo: {hostname}")
             if hostname.startswith("p1meter") or hostname.startswith("kwhmeter"):
-                random.seed(42)
-                serial_number = hash(hostname)
+                serial_number = string_to_int(hostname)
                 logging.info(f"Found HomeWizard meter with hostname: {hostname}, assigned serial number: {serial_number}")
                 with userdata['lock']:
                     userdata['homewizard_meters'][hostname] = serial_number
@@ -41,6 +38,7 @@ def update_homewizard(userdata):
             hostnames = userdata['homewizard_meters']
 
         for (hostname, serial_number) in hostnames.items():
+            packet = emeterPacket(int(serial_number))
             # Perform the GET request
             response = requests.get(f'http://{hostname}/api/v1/data')
             
@@ -93,3 +91,9 @@ def update_homewizard(userdata):
     except Exception as e:
         logging.error(f"An unexpected error occurred: {e}")
     
+def string_to_int(string_value):
+    # Create a SHA-256 hash of the string
+    hash_object = hashlib.sha256(string_value.encode())
+    # Convert the hash to an integer
+    hash_int = int(hash_object.hexdigest(), 16) % (10 ** 8)
+    return hash_int
